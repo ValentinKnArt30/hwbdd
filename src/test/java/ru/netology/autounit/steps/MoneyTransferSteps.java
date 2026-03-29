fd8dcf06684da5b36e1dd6563ee85d59389207af
Valentin Knyshov <v.a.knishov@bk.ru>
2026-03-29 18:48:35 +0300

    FIX: corrections after revision

--- src/test/java/ru/netology/autounit/steps/MoneyTransferSteps.java
+++ src/test/java/ru/netology/autounit/steps/MoneyTransferSteps.java
@@ -1,6 +1,6 @@
 package ru.netology.autounit.steps;
 
-import io.cucumber.java.ru.Допустим;
+import io.cucumber.java.ru.Дано;
 import io.cucumber.java.ru.Когда;
 import io.cucumber.java.ru.Тогда;
 import ru.netology.autounit.data.DataHelper;
@@ -10,7 +10,7 @@ import ru.netology.autounit.page.TransferPage;
 import ru.netology.autounit.page.VerificationPage;
 
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class MoneyTransferSteps {
 
@@ -19,7 +19,7 @@ public class MoneyTransferSteps {
     private DataHelper.CardInfo firstCard;
     private DataHelper.CardInfo secondCard;
 
-    @Допустим("пользователь залогинен с именем {string} и паролем {string}")
+    @Дано("пользователь залогинен с именем {string} и паролем {string}")
     public void login(String username, String password) {
         open("http://localhost:9999");
         authInfo = new DataHelper.AuthInfo(username, password);
@@ -31,16 +31,27 @@ public class MoneyTransferSteps {
         secondCard = DataHelper.getSecondCard();
     }
 
-    @Когда("пользователь переводит {int} рублей с карты с номером {string} на свою 1 карту с главной страницы")
-    public void transferMoney(int amount, String fromCardNumber) {
-        DataHelper.CardInfo fromCard = fromCardNumber.equals(firstCard.getNumber()) ? firstCard : secondCard;
-        TransferPage transferPage = dashboard.selectCardToTopUp(firstCard);
-        dashboard = transferPage.transfer(fromCard, amount);
+    @Когда("пользователь переводит {int} рублей с карты с номером {string} на свою {int} карту с главной страницы")
+    public void transferMoney(int amount, String fromCardNumber, int toCardIndex) {
+        DataHelper.CardInfo fromCard;
+        if (fromCardNumber.equals(firstCard.getNumber())) {
+            fromCard = firstCard;
+        } else if (fromCardNumber.equals(secondCard.getNumber())) {
+            fromCard = secondCard;
+        } else {
+            throw new IllegalArgumentException("Неизвестная карта отправителя: " + fromCardNumber);
+        }
+
+        DataHelper.CardInfo toCard = (toCardIndex == 1) ? firstCard : secondCard;
+
+        TransferPage transferPage = dashboard.selectCardToTopUp(toCard);
+        dashboard = transferPage.validTransfer(fromCard, amount);
     }
 
-    @Тогда("баланс его 1 карты из списка на главной странице должен стать {int} рублей")
-    public void verifyFirstCardBalance(int expectedBalance) {
-        int actualBalance = dashboard.getCardBalance(firstCard);
+    @Тогда("баланс его {int} карты из списка на главной странице должен стать {int} рублей")
+    public void verifyCardBalance(int cardIndex, int expectedBalance) {
+        DataHelper.CardInfo card = (cardIndex == 1) ? firstCard : secondCard;
+        int actualBalance = dashboard.getCardBalance(card);
         assertEquals(expectedBalance, actualBalance);
     }
 }
